6aeb6aacacd3f39e6342531cc02809a77de7e6f0
Blake Jones <82840474+blakee-joness@users.noreply.github.com>
2024-09-09 03:05:39 +0000

    Add RecipeApp project files

---
+++ RecipeApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RecipeApp
+{
+    class Program
+    {
+        static void Main(string[] args) // Entry point of the application
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
