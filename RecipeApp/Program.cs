a9d9798fdf4c901dba2383cfea6a154255614fd3
Blake Jones <82840474+blakee-joness@users.noreply.github.com>
2024-09-09 03:42:22 +0000

    Add xml documentation

--- RecipeApp/Program.cs
+++ RecipeApp/Program.cs
@@ -9,13 +9,37 @@
     // | 1 quart           | 0.95 liters                |
     // | 1 tablespoon      | 14.79 milliliters          |
     // | 1 teaspoon        | 4.93 milliliters           |
+
+
+    /// <summary>
+    /// // Holder of functions for the application
+    /// </summary>
     class Program
     {
-        static void Main(string[] args) // Entry point of the application
+        /// <summary>
+        /// The entry point of the application.
+        /// </summary>
+        static void Main() // Entry point of the application
         {
-            Console.WriteLine("Please enter user input:");
+            Console.WriteLine("Please enter teaspoons amount:");
             string? input = Console.ReadLine();
-            Console.WriteLine($"You entered: {input}");
+
+            Console.WriteLine(
+                $"The conversion of {input} to milliliters is {ConvertTeaspoonsToMilliliters(input)}"
+            );
+        }
+
+        /// <summary>
+        /// Converts the given number of teaspoons to milliliters.
+        /// </summary>
+        /// <param name="teaspoons">The number of teaspoons to convert.</param>
+        /// <returns>The equivalent number of milliliters.</returns>
+        ///
+        static void ConvertTeaspoonsToMilliliters(double teaspoons) // Function to convert Teaspoons to Milliliters
+        {
+            Console.WriteLine("Converting Teaspoons to Milliliters");
+            const double millilitersPerTeaspoon = 4.93;
+            return teaspoons * millilitersPerTeaspoon;
         }
     }
 }
