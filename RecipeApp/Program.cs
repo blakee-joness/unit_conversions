7ca30c254eb0351efe5310bdd053cb97a06f2bd1
Blake Jones <82840474+blakee-joness@users.noreply.github.com>
2024-09-09 03:08:15 +0000

    Add measure reference table as comment.

--- RecipeApp/Program.cs
+++ RecipeApp/Program.cs
@@ -1,5 +1,14 @@
 ﻿namespace RecipeApp
 {
+    // | Unit              | Equivalent in Metric Units |
+    // |-------------------|----------------------------|
+    // | 1 cup             | 0.24 liters                |
+    // | 1 gallon          | 3.79 liters                |
+    // | 1 fluid ounce     | 29.57 milliliters          |
+    // | 1 pint            | 0.47 liters                |
+    // | 1 quart           | 0.95 liters                |
+    // | 1 tablespoon      | 14.79 milliliters          |
+    // | 1 teaspoon        | 4.93 milliliters           |
     class Program
     {
         static void Main(string[] args) // Entry point of the application
